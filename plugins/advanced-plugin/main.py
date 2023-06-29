53472b193b4eb5b9747bcd08395f3db2a2ee00ac
vbakayov93 <113050243+vbakayov93@users.noreply.github.com>
2023-06-29 14:46:01 +0300

    Update main.py

--- plugins/advanced-plugin/main.py
+++ plugins/advanced-plugin/main.py
@@ -3,8 +3,8 @@ from random import randint
 from time import sleep
 from typing import Optional
 
-from engine import PluginCore
-from model import Meta, Device
+# from engine import PluginCore
+# from model import Meta, Device
 
 
 class AdvanceSamplePlugin(PluginCore):
